606c13744dae0897ff0170b8fefeb628ddcad48c
koglern <144786123+koglern@users.noreply.github.com>
2024-12-13 15:30:45 +0100

    Ground Movement

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed;
+    public GameObject otherGround; // Referenz auf den anderen Boden
+    private Renderer background;
+    private float backgroundWidth;
+
     void Start()
     {
-        
+        background = GetComponent<Renderer>();
+        backgroundWidth = background.bounds.size.z; // Z-Länge des Bodens
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Bewegt den Boden kontinuierlich nach hinten
+        transform.Translate(new Vector3(0, 0, -1) * (speed * Time.deltaTime));
+
+        // Wenn der aktuelle Boden hinter die Kamera scrollt, wird er versetzt
+        if (transform.position.z < -backgroundWidth)
+        {
+            Reposition();
+        }
+    }
+
+    void Reposition()
+    {
+        // Setzt den aktuellen Boden vor den anderen Boden
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y,
+            otherGround.transform.position.z + backgroundWidth
+        );
     }
 }
